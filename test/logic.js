63cb79366006e27104081a273fe22a6a99da9fb7
Jonas Verhoelen <jonas.verhoelen@codecentric.de>
2018-10-15 22:36:49 +0200

    upgrade and migrate to node 8.12.0 and composer 0.19.13 and throw out docs stuff

--- test/logic.js
+++ test/logic.js
@@ -20,19 +20,12 @@ describe('#' + namespace, () => {
 
     before(async () => {
         // Embedded connection used for local testing
-        const connectionProfile = {
-            name: 'embedded',
-            'x-type': 'embedded'
-        }
+        const connectionProfile = { name: 'embedded', 'x-type': 'embedded' }
         // Generate certificates for use with the embedded connection
         const credentials = CertificateUtil.generate({ commonName: 'admin' })
 
         // PeerAdmin identity used with the admin connection to deploy business networks
-        const deployerMetadata = {
-            version: 1,
-            userName: 'PeerAdmin',
-            roles: [ 'PeerAdmin', 'ChannelAdmin' ]
-        }
+        const deployerMetadata = { version: 1, userName: 'PeerAdmin', roles: [ 'PeerAdmin', 'ChannelAdmin' ] }
         const deployerCard = new IdCard(deployerMetadata, connectionProfile)
         deployerCard.setCredentials(credentials)
 
@@ -51,18 +44,11 @@ describe('#' + namespace, () => {
         let businessNetworkDefinition = await BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'))
 
         // Install the Composer runtime for the new business network
-        await adminConnection.install(businessNetworkDefinition.getName())
+        await adminConnection.install(businessNetworkDefinition)
 
         // Start the business network and configure an network admin identity
-        const startOptions = {
-            networkAdmins: [
-                {
-                    userName: adminUserName,
-                    enrollmentSecret: 'adminpw'
-                }
-            ]
-        }
-        const adminCards = await adminConnection.start(businessNetworkDefinition, startOptions)
+        const startOptions = { networkAdmins: [ { userName: adminUserName, enrollmentSecret: 'adminpw' } ] }
+        const adminCards = await adminConnection.start(businessNetworkDefinition.getName(), businessNetworkDefinition.getVersion(), startOptions)
 
         // Import the network admin identity for us to use
         adminCardName = `${adminUserName}@${businessNetworkDefinition.getName()}`
